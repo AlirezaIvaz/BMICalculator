892a7204a9eb734622182818490da8a168c7238a
Alireza Ivaz <ivaz1382@gmail.com>
2023-06-19 14:28:31 +0330

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+@file:Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.application)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "ir.alirezaivaz.bmi"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "ir.alirezaivaz.bmi"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
+    implementation(libs.tablericons)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+@file:Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.application) apply false
+    alias(libs.plugins.kotlin) apply false
+}
